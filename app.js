e75f9a2eaae9e2122ba72cf40ee5fab6801a2e9d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-01 18:37:56 +0530

    Adding public directory for serving static files

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world</h1>");
+    res.send("index.html");
 })
 
 server.listen(3000, () => {
