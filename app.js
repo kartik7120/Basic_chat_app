9d66b790cdde8d1cf15097a3c6564e09dd214626
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-01 19:06:58 +0530

    Added some events for socket.io

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const path = require("path");
+const { Server } = require("socket.io");
+const io = new Server(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("index.html");
+})
+
+io.on("connection", (socket) => {
+    console.log("A user is connected");
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("User is disconnected");
+    })
 })
 
 server.listen(3000, () => {
